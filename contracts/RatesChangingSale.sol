a94cb5ce93c938ec52fc983c34bfbeb7ec31d10c
0v1se <dev@0v1se.com>
2018-04-23 12:57:15 +0300

    minor fix

--- contracts/RatesChangingSale.sol
+++ contracts/RatesChangingSale.sol
@@ -31,4 +31,20 @@ contract RatesChangingSale is AbstractSale {
 			setRate(_rates[i].token, _rates[i].rate);
 		}
 	}
+
+	event XPubChange(address token, string xpub);
+	mapping(address => string) xpubs;
+
+	function setXPub(address _token, string _xpub) onlyOwner public {
+		setXPubInternal(_token, _xpub);
+	}
+
+	function setXPubInternal(address _token, string _xpub) internal {
+		xpubs[_token] = _xpub;
+		emit XPubChange(_token, _xpub);
+	}
+
+	function getXPub(address token) constant public returns (string) {
+		return xpubs[token];
+	}
 }
